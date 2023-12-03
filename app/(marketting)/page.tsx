d4aa9f219e12decfbe913cc4584c7bce4472c79a
Krish Das <krishdasatgames@gmail.com>
2023-12-03 11:31:56 +0530

    FIX Problem where hero had scrollbar

--- app/(marketting)/page.tsx
+++ app/(marketting)/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./_component/hero";
 
 export default function Home() {
   return (
-    <main className="flex-1 overflow-scroll p-3">
+    <main className="flex-1 p-3">
       <Hero />
     </main>
   );
