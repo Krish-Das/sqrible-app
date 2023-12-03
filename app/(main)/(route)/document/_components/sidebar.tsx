c10f270af43937c13953fed22822903431562c3c
Krish Das <krishdasatgames@gmail.com>
2023-12-04 00:04:34 +0530

    ADD Sidebar with the handle

--- app/(main)/(route)/document/_components/sidebar.tsx
+++ app/(main)/(route)/document/_components/sidebar.tsx
@@ -1,5 +1,16 @@
 const Sidebar = () => {
-  return <aside className="w-24 bg-muted"></aside>;
+  return (
+    <div className="relative w-56 pr-2">
+      <aside className="h-full bg-muted/50 p-2 py-3">
+        <div className="h-10 w-full rounded-md bg-primary/5" />
+      </aside>
+
+      {/* ------- Handle to resize the sidebar */}
+      <div className="group/handle absolute right-0 top-0 flex h-full w-4 cursor-col-resize  justify-center">
+        <div className="h-full border-l border-primary/[0.1] group-hover/handle:border-l-2 group-hover/handle:border-primary/20" />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
--- app/(main)/(route)/document/page.tsx
+++ app/(main)/(route)/document/page.tsx
@@ -1,5 +1,5 @@
 const DocumentPage = () => {
-  return <main className="flex-1">This is a protected page!</main>;
+  return <main className="flex-1 p-3">This is a protected page!</main>;
 };
 
 export default DocumentPage;
