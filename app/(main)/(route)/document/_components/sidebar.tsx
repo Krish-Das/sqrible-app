e4ef8f77b53acb6d645f6ef463b6b5dbe3765e5d
Krish Das <krishdasatgames@gmail.com>
2023-12-04 00:19:09 +0530

    OPTIMIZE CODE

--- app/(main)/(route)/document/_components/sidebar.tsx
+++ app/(main)/(route)/document/_components/sidebar.tsx
@@ -1,13 +1,13 @@
 const Sidebar = () => {
   return (
-    <div className="relative w-56 pr-2">
-      <aside className="h-full bg-muted/50 p-2 py-3">
+    <div className="relative w-64">
+      <aside className="h-full bg-muted/50 p-3">
         <div className="h-10 w-full rounded-md bg-primary/5" />
       </aside>
 
       {/* ------- Handle to resize the sidebar */}
-      <div className="group/handle absolute right-0 top-0 flex h-full w-4 cursor-col-resize  justify-center">
-        <div className="h-full border-l border-primary/[0.1] group-hover/handle:border-l-2 group-hover/handle:border-primary/20" />
+      <div className="group/handle absolute right-0 top-0 flex h-full w-4  translate-x-1/2 cursor-col-resize justify-center">
+        <div className="h-full border-l border-primary/[0.1] transition group-hover/handle:border-l-2 group-hover/handle:border-primary/20" />
       </div>
     </div>
   );
