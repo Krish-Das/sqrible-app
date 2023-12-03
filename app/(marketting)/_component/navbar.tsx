c0110600dfd9e65f614baca475c021969af4079c
Krish Das <krishdasatgames@gmail.com>
2023-12-03 11:14:20 +0530

    ADD Log-in, Sign-up and Sign-out button

--- app/(marketting)/_component/navbar.tsx
+++ app/(marketting)/_component/navbar.tsx
@@ -1,11 +1,40 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
+import Spinner from "@/components/ui/spinner";
 
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
-    <header className="border-b p-3">
+    <header className="flex h-14 items-center justify-between border-b p-3">
       <Link href="/" className="text-xl font-bold tracking-tight">
         Sqrible.
       </Link>
+
+      <div className="flex items-center gap-2">
+        {isLoading && <Spinner className="mr-5" />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <SignInButton mode="modal">Log In</SignInButton>
+            </Button>
+            <Button variant="outline" size="sm" asChild>
+              <SignUpButton mode="modal">Sign up for free</SignUpButton>
+            </Button>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="outline" size="default" asChild>
+              <Link href="/">Enter Sqrible</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+      </div>
     </header>
   );
 };
---
+++ components/ui/spinner.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+
+const Spinner = ({ className }: { className?: string }) => {
+  return (
+    <div className={cn("animate-spin", className)}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+export default Spinner;
