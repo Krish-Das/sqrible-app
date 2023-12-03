428c7c617e72b7286ea7158a65bc6140c9f826ea
Krish Das <krishdasatgames@gmail.com>
2023-12-03 11:21:08 +0530

    ADD: Hero landing page

--- app/(marketting)/_component/hero.tsx
+++ app/(marketting)/_component/hero.tsx
@@ -1,9 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+
 const Hero = () => {
   return (
-    <div className="w-full h-full grid place-content-center">
-      <p className="text-3xl font-light tracking-tight">
-        Welcome to <span className="font-bold">Sqrible.</span>
-      </p>
+    <div className="grid h-full w-full flex-col place-content-center place-items-center gap-2 text-center">
+      <h1 className="text-2xl font-bold tracking-tight sm:text-3xl">
+        Your Ideas, Documents & Plans. <br /> Unified. Welcome to{" "}
+        <span className="underline underline-offset-2">Sqrible.</span>
+      </h1>
+      <h3 className="leading-tight text-gray-700">
+        Sqrible is the connected workspace where <br /> better, faster work
+        happens.
+      </h3>
+      <Button className="mt-3 w-fit">
+        Enter Sqrible <ArrowRightIcon className="ml-2" />
+      </Button>
     </div>
   );
 };
