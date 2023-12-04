f5a8a55dc30f414b9250e06367d8286c53c3b6fe
Krish Das <krishdasatgames@gmail.com>
2023-12-04 18:55:34 +0530

    ADD User item in the sidebar action item

--- app/(main)/(route)/document/_components/user-item.tsx
+++ app/(main)/(route)/document/_components/user-item.tsx
@@ -1,16 +1,90 @@
 "use client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SignOutButton, useUser } from "@clerk/clerk-react";
+import { CaretSortIcon } from "@radix-ui/react-icons";
 
 const UserItem = () => {
-  return (
-    <>
-      {/* Avatar */}
-      <div className="grid aspect-square h-7 place-items-center rounded-md bg-secondary text-xs font-bold">
-        AI
-      </div>
+  const { user } = useUser();
 
-      <h3 className="truncate text-sm uppercase opacity-70">User Name</h3>
-    </>
+  if (!user) return;
+
+  // @ts-ignore
+  function getUserInitial(user) {
+    // TODO: Check for Null values
+    const firstName = user.firstName;
+    const lastName = user.lastName;
+
+    const initial = firstName[0] + lastName[0];
+    return initial;
+  }
+
+  return (
+    <div className="flex w-full items-center gap-3">
+      <DropdownMenu>
+        <DropdownMenuTrigger className="w-full" asChild>
+          <Button className="flex justify-between" variant="secondary">
+            <UserDetails user={user} userInitial={getUserInitial(user)} />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-80" align="start" forceMount>
+          <div className="flex flex-col gap-3 p-3">
+            <p className="text-xs font-medium leading-none text-muted-foreground">
+              {user.emailAddresses[0].emailAddress}
+            </p>
+            <div className="flex items-center gap-2">
+              <Avatar className="h-8 w-8">
+                <AvatarImage src={user.imageUrl} />
+                <AvatarFallback className="text-xs font-bold">
+                  {getUserInitial(user)}
+                </AvatarFallback>
+              </Avatar>
+
+              <p className="line-clamp-1 text-sm">
+                {user.fullName}&apos;s Sqrible
+              </p>
+            </div>
+          </div>
+
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Button asChild className="w-full" variant="secondary">
+              <SignOutButton />
+            </Button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 };
 
 export default UserItem;
+
+// @ts-ignore //TODO: Fix this
+function UserDetails({ user, userInitial }: { user; userInitial: string }) {
+  return (
+    <>
+      <div className="flex items-center justify-start gap-2">
+        <Avatar className="h-6 w-6">
+          <AvatarImage src={user.imageUrl} />
+          <AvatarFallback className="text-xs font-bold">
+            {userInitial}
+          </AvatarFallback>
+        </Avatar>
+
+        <h3 className="text-star tracking line-clamp-1 truncate text-base font-bold opacity-80">
+          {user.fullName}
+        </h3>
+      </div>
+
+      <CaretSortIcon className="justify-self-end" />
+    </>
+  );
+}
