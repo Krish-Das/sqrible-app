559889a5b4c99145a898490910e6e1565ce8673d
Krish Das <krishdasatgames@gmail.com>
2023-12-04 17:10:34 +0530

    ADD Hero page for document page

---
+++ app/(main)/(route)/document/_components/hero.tsx
@@ -0,0 +1,5 @@
+const Hero = () => {
+  return <>This is hero page</>;
+};
+
+export default Hero;
--- app/(main)/(route)/document/page.tsx
+++ app/(main)/(route)/document/page.tsx
@@ -1,5 +1,11 @@
+import Hero from "./_components/hero";
+
 const DocumentPage = () => {
-  return <main className="flex-1 truncate">This is a protected Page</main>;
+  return (
+    <main className="grid flex-1 place-items-center truncate">
+      <Hero />
+    </main>
+  );
 };
 
 export default DocumentPage;
