e7a2e14e0c2b41eb04eb0c83cb7332e3ea6a308a
Krish Das <krishdasatgames@gmail.com>
2023-12-04 11:22:13 +0530

    ADD Action item in the sidebar

--- app/(main)/(route)/document/_components/sidebar.tsx
+++ app/(main)/(route)/document/_components/sidebar.tsx
@@ -1,16 +1,34 @@
-const Sidebar = () => {
-  return (
-    <div className="relative w-64">
-      <aside className="h-full bg-muted/50 p-3">
-        <div className="h-10 w-full rounded-md bg-primary/5" />
-      </aside>
+import { Button } from "@/components/ui/button";
+import { ChevronLeftIcon } from "@radix-ui/react-icons";
 
-      {/* ------- Handle to resize the sidebar */}
-      <div className="group/handle absolute right-0 top-0 flex h-full w-4  translate-x-1/2 cursor-col-resize justify-center">
-        <div className="h-full border-l border-primary/[0.1] transition group-hover/handle:border-l-2 group-hover/handle:border-primary/20" />
+const Sidebar = () => (
+  <div className="relative w-64">
+    <aside className="h-full bg-muted/50 p-3">
+      {/* Action Items */}
+      <div className="flex w-full cursor-pointer select-none items-center justify-between rounded-md p-1 transition hover:bg-primary/5">
+        <div className="flex items-center gap-3">
+          {/* Avatar */}
+          <div className="grid aspect-square h-7 place-items-center rounded-md bg-secondary text-xs font-bold">
+            AI
+          </div>
+
+          <h3 className="text-sm uppercase opacity-70">Action Items</h3>
+        </div>
+
+        {/* Collapse sidebar */}
+        <Button size="icon" variant="ghost">
+          <ChevronLeftIcon />
+        </Button>
       </div>
+    </aside>
+
+    {/* ------- Handle to resize the sidebar */}
+    {/* increased padding for better user experience */}
+    <div className="group/handle absolute right-0 top-0 flex h-full w-4  translate-x-1/2 cursor-col-resize justify-center">
+      {/* ------- Actual handle */}
+      <div className="h-full border-l border-primary/[0.1] transition group-hover/handle:border-l-2 group-hover/handle:border-primary/20" />
     </div>
-  );
-};
+  </div>
+);
 
 export default Sidebar;
