125e9d27c19fd9eca4343b40a493113f6a1932a4
Krish Das <krishdasatgames@gmail.com>
2023-12-06 16:25:13 +0530

    ADD Create document function and IMPLEMENT Create new note button

--- app/(main)/(route)/document/_components/hero.tsx
+++ app/(main)/(route)/document/_components/hero.tsx
@@ -2,10 +2,28 @@
 import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 import { PlusCircledIcon } from "@radix-ui/react-icons";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 const Hero = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  function handleCreateNote() {
+    const promise = create({ title: "Untitiled" });
+
+    toast.promise(
+      promise,
+      // Define states
+      {
+        loading: "Createing new note...",
+        success: "New note created.",
+        error: "Failed to create a new note!",
+      },
+    );
+  }
 
   if (!user) return;
 
@@ -15,9 +33,9 @@ const Hero = () => {
       <h2 className="text-lg font-medium">
         Welcome to {user.firstName}&apos;s space
       </h2>
-      <Button>
+      <Button onClick={handleCreateNote}>
         <PlusCircledIcon className="mr-2" />
-        <p>Create a note</p>
+        Create a note
       </Button>
     </div>
   );
